30fe75f1974df62e86b35115bdb63a623a76219d
hbc <hbcdec@gmail.com>
2018-07-06 15:46:25 +0800

    update scripts

--- crop.py
+++ crop.py
@@ -4,16 +4,15 @@ import os
 import pylab as plb
 import PIL.Image as Image
 
-import os
 #读取path路径下的 jpg文件
-def getAllFrames(dir):
+def getAllFiles(dir):
     #f.endswith（）  限制文件类型
     #f.endswith('.jpg')|f.endswith('.png')  改成这句可以读取jpg/png格式的文件
     #注意 返回的是绝对路径
    return [os.path.join(dir,f) for f in os.listdir(dir) if f.endswith('.jpg')]
 
 #循环读图
-for dir in getAllFrames(r'/home/hbc/data/smartcity/dataset3/Cam5/frames/'):
+for dir in getAllFiles(r'/home/hbc/data/smartcity/dataset3/Cam5/frames/'):
     frame = Image.open(dir)
     label = os.path.splitext(dir.replace('frames','labels'))[0]+'.txt'
     print ('in '+label)
--- video2frames.py
+++ video2frames.py
@@ -1,22 +1,19 @@
 import cv2
 f=0
-c=0
-cap = cv2.VideoCapture('/home/hbc/data/smartcity/dataset3/Cam5/Cam5.avi')
+cap = cv2.VideoCapture('/home/hbc/data/smartcity/dataset1/Cam3/Cam3.avi')
 if cap.isOpened():
     rval,frame = cap.read()
 else:
     rval = False
 while rval:
     rows,cols,channel = frame.shape
-    #Set the frame rate
-    if(f%5 == 0):
-        _path = '/home/hbc/data/smartcity/dataset3/Cam5/frames/'+str(c).zfill(6)+'.jpg'
-        cv2.imencode('.jpg',frame)[1].tofile(_path)  
-        print ('saving %s' % _path)
-        c = c + 1
-        cv2.waitKey(1)
+    _path = '/home/hbc/data/smartcity/frames/'+'1_3_'+str(f).zfill(6)+'.jpg'
+    # '.jpg'表示把当前图片frame按照jpg格式编码，按照不同格式编码的结果不一样
+    cv2.imencode('.jpg',frame)[1].tofile(_path)  
+    print ('saving %s' % _path)
+    cv2.waitKey(1)
     f = f + 1
     rval,frame = cap.read()
 cap.release()
-print ('total saved %d imagess' % c)
+print ('total saved %d imagess' % f)
 
