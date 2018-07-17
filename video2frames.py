809f3434563bedf971d855e72dd45f5fb3761d32
hbc <hbcdec@gmail.com>
2018-07-17 10:05:14 +0800

    update

--- video2frames.py
+++ video2frames.py
@@ -1,13 +1,13 @@
 import cv2
 f=0
-cap = cv2.VideoCapture('/home/hbc/data/smartcity/dataset1/Cam3/Cam3.avi')
+cap = cv2.VideoCapture('/home/hbc/data/smartcity/dataset3/Cam5/Cam5.avi')
 if cap.isOpened():
     rval,frame = cap.read()
 else:
     rval = False
 while rval:
     rows,cols,channel = frame.shape
-    _path = '/home/hbc/data/smartcity/frames/'+'1_3_'+str(f).zfill(6)+'.jpg'
+    _path = '/home/hbc/data/smartcity/frames/'+'3_5_'+str(f).zfill(6)+'.jpg'
     # '.jpg'表示把当前图片frame按照jpg格式编码，按照不同格式编码的结果不一样
     cv2.imencode('.jpg',frame)[1].tofile(_path)  
     print ('saving %s' % _path)
